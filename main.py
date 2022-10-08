d3be25c7171eb557fa8601c31c190d88d6eeab0d
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-08 13:35:08 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-
-# ******************************
-# Make your Code
-# ******************************
 import random
 
 numbers1 = []
 numbers2 = []
 result = []
 for i in range(10):
-	numbers1.append(random.randint(0,100))
-	numbers2.append(random.randint(0,100))
-print (numbers1)
-print (numbers2)
+	numbers1.append(random.randint(0,10))
+	numbers2.append(random.randint(0,10))
+	result.append(numbers1[len(numbers1)-1] + numbers2[len(numbers2)-1])
+print(numbers1)
+print("+")
+print(numbers2)
+print("=")
+print(result)
